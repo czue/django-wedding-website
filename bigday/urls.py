4289ed8d8ceba375eb3bd8c74e13815d93a3cf76
Cory Zue <czue@dimagi.com>
2016-01-02 18:06:14 -0500

    add favicon

--- bigday/urls.py
+++ bigday/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^guests/', include('guests.urls')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
     url(r'^admin/', admin.site.urls),
 ]
