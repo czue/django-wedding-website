1686e1df64e74e3a57c9fdc1cba3cb4a08a241a0
Cory Zue <czue@dimagi.com>
2016-01-02 17:31:12 -0500

    add fontawesome, creative dependencies, convert to creative template

--- guests/views.py
+++ guests/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'base.html')
+    # return render(request, 'base.html')
+    return render(request, 'base2.html')
