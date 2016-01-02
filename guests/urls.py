8026615b8f4fde8bda7aa857e713b91b101bce70
Cory Zue <czue@dimagi.com>
2016-01-02 18:06:51 -0500

    add stub story page

--- guests/urls.py
+++ guests/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^story/$', views.story, name='story'),
 ]
--- guests/views.py
+++ guests/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
     # return render(request, 'base.html')
-    return render(request, 'base2.html')
+    return render(request, 'home.html')
+
+
+def story(request):
+    # return render(request, 'base.html')
+    return render(request, 'story.html')
