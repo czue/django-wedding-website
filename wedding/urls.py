8788fb19dc45b8df7b879b5777a1a6f4cb302b0f
Cory Zue <czue@dimagi.com>
2016-01-02 19:26:15 -0500

    remove story

--- wedding/urls.py
+++ wedding/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^story/$', views.story, name='story'),
 ]
--- wedding/views.py
+++ wedding/views.py
@@ -4,7 +4,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
-
-
-def story(request):
-    return render(request, 'story.html')
