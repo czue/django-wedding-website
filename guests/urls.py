f9b67a9ac252b01d991c4d045955a8f2b8005e46
Cory Zue <czue@dimagi.com>
2016-01-10 17:33:11 -0500

    add save the date view

--- guests/urls.py
+++ guests/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 
 from . import views
+from guests.views import GuestListView, EmailPreviewView
 
 urlpatterns = [
+    url(r'^$', GuestListView.as_view(), name='guest-list'),
+    url(r'^save-the-date/', EmailPreviewView.as_view(), name='guest-list'),
 ]
--- guests/views.py
+++ guests/views.py
@@ -1,1 +1,11 @@
-# todo: nothing here yet
+from django.views.generic import ListView, TemplateView
+from guests.models import Guest
+
+
+class GuestListView(ListView):
+    model = Guest
+
+
+class EmailPreviewView(TemplateView):
+    template_name = 'guests/email_templates/save_the_date.html'
+
