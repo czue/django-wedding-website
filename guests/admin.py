96e35348f4333ed1d8d889a95212263f65e4089c
Cory Zue <czue@dimagi.com>
2016-01-10 17:30:45 -0500

    add guests app, stub models file

--- guests/admin.py
+++ guests/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Guest
 
-# Register your models here.
+class GuestAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'email', 'save_the_date_sent', 'save_the_date_opened', 'invited', 'attending')
+    list_filter = ('save_the_date_sent', 'save_the_date_opened', 'invited', 'attending')
+
+
+admin.site.register(Guest, GuestAdmin)
+
+
--- guests/models.py
+++ guests/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Guest(models.Model):
+
+    name = models.TextField()
+    email = models.TextField()
+    save_the_date_sent = models.BooleanField(default=False)
+    save_the_date_opened = models.BooleanField(default=False)
+    invited = models.BooleanField(default=False)
+    attending = models.NullBooleanField(default=None)
+
+    def __unicode__(self):
+        return self.name
