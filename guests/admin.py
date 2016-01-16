5a65b93ef30de921165100250c39736b1df0cb97
Cory Zue <czue@dimagi.com>
2016-01-16 12:01:42 -0500

    update models to split party from guests

--- guests/admin.py
+++ guests/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Guest
+from .models import Guest, Party
+
+
+class PartyAdmin(admin.ModelAdmin):
+    pass
+
 
 class GuestAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'email', 'save_the_date_sent', 'save_the_date_opened', 'invited', 'attending')
-    list_filter = ('save_the_date_sent', 'save_the_date_opened', 'invited', 'attending')
+    list_display = ('party', 'first_name', 'last_name', 'email', 'is_attending')
+    list_filter = ('is_attending',)
 
 
+admin.site.register(Party, PartyAdmin)
 admin.site.register(Guest, GuestAdmin)
 
 
--- guests/models.py
+++ guests/models.py
@@ -3,14 +3,36 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Guest(models.Model):
+# these will determine the default formality of correspondence
+ALLOWED_TYPES = [
+    ('formal', 'formal'),
+    ('fun', 'fun')
+]
 
+
+class Party(models.Model):
+    """
+    A party consists of one or more guests.
+    """
     name = models.TextField()
-    email = models.TextField()
-    save_the_date_sent = models.BooleanField(default=False)
-    save_the_date_opened = models.BooleanField(default=False)
-    invited = models.BooleanField(default=False)
-    attending = models.NullBooleanField(default=None)
+    type = models.CharField(max_length=10, choices=ALLOWED_TYPES)
+    save_the_date_sent = models.DateTimeField(null=True, default=None)
+    save_the_date_opened = models.DateTimeField(null=True, default=None)
+    is_invited = models.BooleanField(default=False)
+    is_attending = models.NullBooleanField(default=None)
 
     def __unicode__(self):
-        return self.name
+        return 'Party: {}'.format(self.name)
+
+class Guest(models.Model):
+    """
+    A single guest
+    """
+    party = models.ForeignKey(Party)
+    first_name = models.TextField()
+    last_name = models.TextField(null=True, blank=True)
+    email = models.TextField()
+    is_attending = models.NullBooleanField(default=None)
+
+    def __unicode__(self):
+        return 'Guest: {} {}'.format(self.first_name, self.last_name)
