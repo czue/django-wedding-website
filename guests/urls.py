6be8df41a9f4b6cce699ee1adaf6f00983bd8168
Cory Zue <czue@dimagi.com>
2016-01-17 19:16:54 +0200

    dry up the views

--- guests/urls.py
+++ guests/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
 
 from . import views
-from guests.views import GuestListView, test_email, RoEmailPreviewView, CoryEmailPreviewView
+from guests.views import GuestListView, test_email, save_the_date_preview
 
 urlpatterns = [
     url(r'^$', GuestListView.as_view(), name='guest-list'),
-    url(r'^save-the-date/', RoEmailPreviewView.as_view(), name='save-the-date'),
-    url(r'^save-the-date-cory/', CoryEmailPreviewView.as_view(), name='save-the-date-cory'),
-    url(r'^email-test/', test_email, name='test-email'),
+    url(r'^save-the-date/(?P<template_id>[\w-]+)/$', save_the_date_preview, name='save-the-date'),
+    url(r'^email-test/(?P<template_id>[\w-]+)/$', test_email, name='test-email'),
 ]
--- guests/views.py
+++ guests/views.py
@@ -4,6 +4,7 @@ import os
 from django.core.mail import EmailMultiAlternatives
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.views.generic import ListView, TemplateView
 from postmark import PMMail
@@ -14,32 +15,38 @@ class GuestListView(ListView):
     model = Guest
 
 
-class RoEmailPreviewView(TemplateView):
-    template_name = 'guests/email_templates/save_the_date.html'
+SAVE_THE_DATE_TEMPLATE = 'guests/email_templates/save_the_date.html'
 
-    def get_context_data(self, **kwargs):
-        return {
+
+def save_the_date_preview(request, template_id):
+    return render(request, SAVE_THE_DATE_TEMPLATE, context=_get_save_the_date_context(template_id))
+
+
+def _get_save_the_date_context(template_id):
+    context_map = {
+        'classy': {
             'header_filename': 'hearts.png',
             'main_image': 'selfie.jpg',
             'main_color': '#fff3e8',
             'font_color': '#666666',
-        }
-
-
-class CoryEmailPreviewView(TemplateView):
-    template_name = 'guests/email_templates/save_the_date.html'
-
-    def get_context_data(self, **kwargs):
-        return {
+        },
+        'canada': {
             'header_filename': 'maple-leaf.png',
             'main_image': 'canada-cartoon.jpg',
             'main_color': '#ea2e2e',
             'font_color': '#e5ddd9',
         }
+    }
+    template_id = (template_id or '').lower()
+    if template_id not in context_map:
+        template_id = 'classy'
+    return context_map[template_id]
 
 
-def test_email(request):
-    template_html = render_to_string('guests/email_templates/save_the_date_ro.html', context={'email_mode': True})
+def test_email(request, template_id):
+    context = _get_save_the_date_context(template_id)
+    context['email_mode'] = True
+    template_html = render_to_string(SAVE_THE_DATE_TEMPLATE, context=context)
     template_text = 'sorry, you need to view this in html mode'
     subject = 'save the date!'
     recipients = ['cory.zue@gmail.com']
@@ -47,7 +54,7 @@ def test_email(request):
     msg = EmailMultiAlternatives(subject, template_text, 'hello@coryandro.com', recipients)
     msg.attach_alternative(template_html, "text/html")
     msg.mixed_subtype = 'related'
-    for filename in ('hearts.png', 'selfie.jpg'):
+    for filename in (context['header_filename'], context['main_image']):
         attachment_path = os.path.join(os.path.dirname(__file__), 'static', 'save-the-date', 'images', filename)
         with open(attachment_path, "rb") as image_file:
             msg_img = MIMEImage(image_file.read())
@@ -56,7 +63,6 @@ def test_email(request):
 
     msg.send()
     return HttpResponse('sent!')
-    # return HttpResponseRedirect(reverse('save-the-date'))
 
 
 def test_postmark_email(request):
