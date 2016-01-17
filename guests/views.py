6c2513d3445b0fda2c7e38e299b6335ea1881135
Cory Zue <czue@dimagi.com>
2016-01-17 17:41:33 +0200

    send with django's built ins

--- guests/views.py
+++ guests/views.py
@@ -1,7 +1,9 @@
 import base64
+from email.mime.image import MIMEImage
 import os
+from django.core.mail import EmailMultiAlternatives
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template.loader import render_to_string
 from django.views.generic import ListView, TemplateView
 from postmark import PMMail
@@ -20,6 +22,27 @@ class CoryEmailPreviewView(TemplateView):
 
 
 def test_email(request):
+    template_html = render_to_string('guests/email_templates/save_the_date_ro.html', context={'email_mode': True})
+    template_text = 'sorry, you need to view this in html mode'
+    subject = 'save the date!'
+    recipients = ['cory.zue@gmail.com']
+    # https://www.vlent.nl/weblog/2014/01/15/sending-emails-with-embedded-images-in-django/
+    msg = EmailMultiAlternatives(subject, template_text, 'hello@coryandro.com', recipients)
+    msg.attach_alternative(template_html, "text/html")
+    msg.mixed_subtype = 'related'
+    for filename in ('hearts.png', 'selfie.jpg'):
+        attachment_path = os.path.join(os.path.dirname(__file__), 'static', 'save-the-date', 'images', filename)
+        with open(attachment_path, "rb") as image_file:
+            msg_img = MIMEImage(image_file.read())
+            msg_img.add_header('Content-ID', '<{}>'.format(filename))
+            msg.attach(msg_img)
+
+    msg.send()
+    return HttpResponse('sent!')
+    # return HttpResponseRedirect(reverse('save-the-date'))
+
+
+def test_postmark_email(request):
     template_text = render_to_string('guests/email_templates/save_the_date.html', context={'email_mode': True})
     attachments = []
     for filename in ('hearts.png', 'selfie.jpg'):
@@ -40,6 +63,6 @@ def test_email(request):
     return HttpResponseRedirect(reverse('save-the-date'))
 
 
-def _base64_encode(file_path):
-    with open(file_path, "rb") as image_file:
+def _base64_encode(filepath):
+    with open(filepath, "rb") as image_file:
         return base64.b64encode(image_file.read())
