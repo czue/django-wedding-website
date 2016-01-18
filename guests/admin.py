115e1180cf76b4f4583c269a38e75464c4993c12
Cory Zue <czue@dimagi.com>
2016-01-18 21:46:19 +0200

    better party admin

--- guests/admin.py
+++ guests/admin.py
@@ -3,16 +3,14 @@ from .models import Guest, Party
 
 
 class PartyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'type', 'category')
+    list_filter = ('type', 'category')
 
 
 class GuestAdmin(admin.ModelAdmin):
-
     list_display = ('party', 'first_name', 'last_name', 'email', 'is_attending')
     list_filter = ('is_attending',)
 
 
 admin.site.register(Party, PartyAdmin)
 admin.site.register(Guest, GuestAdmin)
-
-
