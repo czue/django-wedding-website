59f6b9d65da49c661aedd7cc4c4a32c2618e2c61
Cory Zue <czue@dimagi.com>
2016-01-19 21:41:25 +0200

    make random save the date the default landing page

--- bigday/urls.py
+++ bigday/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'', include('wedding.urls')),
-    url(r'^guests/', include('guests.urls')),
+    url(r'public/', include('wedding.urls')),
+    url(r'^', include('guests.urls')),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
     url(r'^admin/', admin.site.urls),
     url('^accounts/', include('django.contrib.auth.urls'))
--- guests/urls.py
+++ guests/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
 from . import views
-from guests.views import GuestListView, test_email, save_the_date_preview
+from guests.views import GuestListView, test_email, save_the_date_preview, save_the_date_random
 
 urlpatterns = [
-    url(r'^$', GuestListView.as_view(), name='guest-list'),
+    url(r'^$', save_the_date_random, name='save-the-date-random'),
+    url(r'^guests/$', GuestListView.as_view(), name='guest-list'),
     url(r'^save-the-date/(?P<template_id>[\w-]+)/$', save_the_date_preview, name='save-the-date'),
     url(r'^email-test/(?P<template_id>[\w-]+)/$', test_email, name='test-email'),
 ]
