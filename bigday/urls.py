282af97f8bc8c41f8a0c63ae5ba86fe4c5fcf11b
Cory Zue <czue@dimagi.com>
2016-02-28 18:15:14 +0200

    ready for launch!

--- bigday/urls.py
+++ bigday/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'public/', include('wedding.urls')),
+    url(r'^', include('wedding.urls')),
     url(r'^', include('guests.urls')),
     url(r'^email-images/', include('email_tracker.urls')),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
