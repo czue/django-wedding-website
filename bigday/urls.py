7b43cd8c8687a7f7c4b6066753c203bc4b5961e8
Cory Zue <czue@dimagi.com>
2016-03-20 12:26:10 +0200

    better favicon support

--- bigday/urls.py
+++ bigday/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import RedirectView
+
 
 urlpatterns = [
     url(r'^', include('wedding.urls')),
     url(r'^', include('guests.urls')),
     url(r'^email-images/', include('email_tracker.urls')),
-    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
     url(r'^admin/', admin.site.urls),
     url('^accounts/', include('django.contrib.auth.urls'))
 ]
