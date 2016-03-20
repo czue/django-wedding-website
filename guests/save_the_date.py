5f77d8c0991ff472fd370adc85d3387b162d4c90
Cory Zue <czue@dimagi.com>
2016-03-20 16:54:16 +0200

    pull out preheader text

--- guests/save_the_date.py
+++ guests/save_the_date.py
@@ -104,6 +104,10 @@ def get_save_the_date_context(template_id):
     context = copy(SAVE_THE_DATE_CONTEXT_MAP[template_id])
     context['name'] = template_id
     context['page_title'] = 'Cory and Rowena - Save the Date!'
+    context['preheader_text'] = (
+        "The date that you've eagerly been waiting for is finally here. "
+        "Cory and Ro are getting married! Save the date!"
+    )
     return context
 
 
