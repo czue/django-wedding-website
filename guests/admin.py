67ca067d254de4e20292d5d90485c2fa3d7504ac
Cory Zue <czue@dimagi.com>
2016-03-27 16:15:05 +0200

    add inline guests to party admin view

--- guests/admin.py
+++ guests/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from .models import Guest, Party
 
 
+class GuestInline(admin.TabularInline):
+    model = Guest
+    readonly_fields = ('first_name', 'last_name', 'email', 'is_attending', 'meal', 'is_child')
+
+
 class PartyAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'category', 'save_the_date_sent', 'is_invited', 'is_attending')
     list_filter = ('type', 'category', 'is_invited', 'is_attending')
+    inlines = [GuestInline]
 
 
 class GuestAdmin(admin.ModelAdmin):
