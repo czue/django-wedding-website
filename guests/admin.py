b5fc9970a6e3d1bc9725a79366de97b9f979f8e2
Cory Zue <czue@dimagi.com>
2016-05-19 11:41:34 -0400

    add rehearsal dinner to admin view

--- guests/admin.py
+++ guests/admin.py
@@ -9,7 +9,7 @@ class GuestInline(admin.TabularInline):
 
 
 class PartyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'category', 'save_the_date_sent', 'invitation_sent', 'invitation_opened',
+    list_display = ('name', 'type', 'category', 'save_the_date_sent', 'invitation_sent', 'rehearsal_dinner', 'invitation_opened',
                     'is_invited', 'is_attending')
     list_filter = ('type', 'category', 'is_invited', 'is_attending', 'rehearsal_dinner', 'invitation_opened')
     inlines = [GuestInline]
