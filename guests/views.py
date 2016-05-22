4456f232e8671f8c9ff7771cb66785665da4a744
Cory Zue <czue@dimagi.com>
2016-05-22 10:47:21 -0400

    add breakdowns to the dashboard

--- guests/views.py
+++ guests/views.py
@@ -5,6 +5,7 @@ import random
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.db.models import Count
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
@@ -37,16 +38,25 @@ def dashboard(request):
     ).order_by('category', 'name')
     parties_with_unopen_invites = parties_with_pending_invites.filter(invitation_opened=None)
     parties_with_open_unresponded_invites = parties_with_pending_invites.exclude(invitation_opened=None)
+    attending_guests = Guest.objects.filter(is_attending=True)
+    guests_without_meals = attending_guests.filter(is_child=False, meal=None).order_by(
+        'party__category', 'first_name'
+    )
+    meal_breakdown = attending_guests.exclude(meal=None).values('meal').annotate(count=Count('*'))
+    category_breakdown = attending_guests.values('party__category').annotate(count=Count('*'))
     return render(request, 'guests/dashboard.html', context={
         'guests': Guest.objects.filter(is_attending=True).count(),
         'possible_guests': Guest.objects.filter(party__is_invited=True).exclude(is_attending=False).count(),
         'not_coming_guests': Guest.objects.filter(is_attending=False).count(),
         'pending_invites': parties_with_pending_invites.count(),
         'pending_guests': Guest.objects.filter(party__is_invited=True, is_attending=None).count(),
+        'guests_without_meals': guests_without_meals,
         'parties_with_unopen_invites': parties_with_unopen_invites,
         'parties_with_open_unresponded_invites': parties_with_open_unresponded_invites,
         'unopened_invite_count': parties_with_unopen_invites.count(),
         'total_invites': Party.objects.filter(is_invited=True).count(),
+        'meal_breakdown': meal_breakdown,
+        'category_breakdown': category_breakdown,
     })
 
 
