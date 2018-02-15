67a72722d401678663a2ab0684fb1c5bf9f37f27
Augustin Borsu <a.borsu@gmail.com>
2018-02-15 11:22:43 +0100

    feat: internationalization

--- bigday/settings.py
+++ bigday/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+from django.utils.translation import ugettext_lazy as _
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -65,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -116,6 +120,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+LANGUAGES = (
+    ('en', _('English')),
+    ('es', _('Spanish')),
+    ('fr', _('French')),
+)
+
+LOCALE_PATHS = (
+    os.path.join('bigday', 'locale'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
--- bigday/urls.py
+++ bigday/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url, include
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
+# urlpatterns = [
+#     url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+#         home_files, name='home-files'),
+# ]
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     url(r'^', include('wedding.urls')),
     url(r'^', include('guests.urls')),
     url(r'^admin/', admin.site.urls),
     url('^accounts/', include('django.contrib.auth.urls'))
-]
+)
