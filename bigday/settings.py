b6495f6fee23505ce3ae7283f7eddbf3dd71fda8
Augustin Borsu <a.borsu@gmail.com>
2018-02-16 14:56:51 +0100

    Reboot landing page

--- bigday/settings.py
+++ bigday/settings.py
@@ -133,7 +133,7 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = 'static_root'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join('bigday', 'static'),
