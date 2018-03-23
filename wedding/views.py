313d8fa0c4cb551ffa3cc0fedc76859ca9ecae27
Cory Zue <czue@dimagi.com>
2018-03-23 14:25:26 +0200

    use default reply email on the homepage

--- wedding/views.py
+++ wedding/views.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.shortcuts import render
 from guests.save_the_date import SAVE_THE_DATE_CONTEXT_MAP
 
 
 def home(request):
     return render(request, 'home.html', context={
-        'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP
+        'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
+        'support_email': settings.DEFAULT_WEDDING_REPLY_EMAIL,
     })
