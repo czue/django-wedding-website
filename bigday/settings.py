9e6802e422d9d47e2e7eaae6e555975a2f683566
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-07-02 01:51:27 -0300

    heroku requirements

--- bigday/settings.py
+++ bigday/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -139,3 +141,5 @@ try:
     from .localsettings import *
 except ImportError:
     pass
+
+django_heroku.settings(locals())
