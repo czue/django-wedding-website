863f03e7e7a16dfc2a1caf914af1a0b1c4477aa0
adrien <grrd.adrien@gmail.com>
2018-11-24 15:12:51 +0100

    Feed gallery with pictures set in photologue/photos directory

--- bigday/views.py
+++ bigday/views.py
@@ -1,8 +1,13 @@
+import os
 from django.shortcuts import render
-
+from django.conf import settings
 
 def home(request):
     return render(request, 'home.html')
 
+def get_file_names(url):
+    return os.listdir(url)
+
 def gallery(request):
-    return render(request, 'gallery.html')
+    picture_names = list(filter(lambda x: x != 'cache', get_file_names("media/photologue/photos")))
+    return render(request, 'gallery.html', {'photos': picture_names})
