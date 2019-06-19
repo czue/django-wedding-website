13a2d3506875c470144be5bd145c1320a3da16e9
Cory Zue <czue@dimagi.com>
2019-06-19 13:03:26 +0200

    make default email backend console

--- bigday/settings.py
+++ bigday/settings.py
@@ -135,6 +135,9 @@ DEFAULT_WEDDING_REPLY_EMAIL = 'happilyeverafter@example.com'
 DEFAULT_WEDDING_TEST_EMAIL = DEFAULT_WEDDING_FROM_EMAIL
 WEDDING_CC_LIST = []  # put email addresses here if you want to cc someone on all your invitations
 
+# change to a real email backend in production
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 try:
     from .localsettings import *
 except ImportError:
