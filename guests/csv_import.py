6f6e0b634878e8f0b4be0069ef511a871d9bd342
Cory Zue <czue@dimagi.com>
2019-06-19 11:04:41 +0200

    fix csv import for py3

--- guests/csv_import.py
+++ guests/csv_import.py
@@ -9,7 +9,7 @@ except ImportError:
 
 
 def import_guests(path):
-    with open(path, 'rb') as csvfile:
+    with open(path, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         first_row = True
         for row in reader:
