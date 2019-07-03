e60cf779bec042f667e2141ec8b430baffe9c232
Adrien Gerard <agerard@altissia.org>
2019-07-03 20:42:16 +0200

    fix filter pictures by prefixes

--- bigday/views.py
+++ bigday/views.py
@@ -1,25 +1,28 @@
 import os
-from django.shortcuts import render
+
 from django.conf import settings
+from django.shortcuts import render
+
 
 def home(request):
     return render(request, 'home.html')
 
-def get_file_names(url):
-    return os.listdir(url)
-
+def get_file_names_by_prefix(url, prefix):
+    list_filenames = os.listdir(url)
+    finalList = []
+    for filename in list_filenames:
+        if filename.startswith(prefix):
+            finalList.append(filename)
+    return finalList
 
 def g_home(request):
     return render(request, 'gallery-home.html')
 
 def g_pro(request):
-    picture_names = list(filter(lambda x: x != 'cache', get_file_names("media/photologue/photos/pro")))
-    return render(request, 'gallery.html', {'photos': picture_names})
+    return render(request, 'gallery.html', {'photos': get_file_names_by_prefix("media/photologue/photos", 'pro_')})
 
 def g_perso(request):
-    picture_names = list(filter(lambda x: x != 'cache', get_file_names("media/photologue/photos/perso")))
-    return render(request, 'gallery.html', {'photos': picture_names})
+    return render(request, 'gallery.html', {'photos': get_file_names_by_prefix("media/photologue/photos", 'friends_')})
 
 def g_other(request):
-    picture_names = list(filter(lambda x: x != 'cache', get_file_names("media/photologue/photos/other")))
-    return render(request, 'gallery.html', {'photos': picture_names})
+    return render(request, 'gallery.html', {'photos': get_file_names_by_prefix("media/photologue/photos", 'others_')})
