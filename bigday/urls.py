5cb12b6615cf2fcf408e80d5fecf3cca7c93798f
Guillem Gelabert Mestre <ggelabert@hotelbeds.com>
2020-02-01 18:54:22 +0100

    Add url translation

--- bigday/urls.py
+++ bigday/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-
 urlpatterns = [
     url(r'^', include('wedding.urls')),
     url(r'^', include('guests.urls')),
     url(r'^admin/', admin.site.urls),
-    url('^accounts/', include('django.contrib.auth.urls'))
+    url('^accounts/', include('django.contrib.auth.urls')),
+    url('i18n/', include('django.conf.urls.i18n'))
 ]
