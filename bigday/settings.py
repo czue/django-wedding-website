cdb04f5393e3553ce04ea22e43a2b4ceac2a389e
Guillem Gelabert Mestre <ggelabert@hotelbeds.com>
2020-02-03 22:19:32 +0100

    Fixed language configuration and switch

--- bigday/settings.py
+++ bigday/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'u7!-y4k1c6b44q507nr_l+c^12o7ur++cpzyn!$65w^!gum@h%'
 DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.1.35', '127.0.0.1']
-
 
 # Application definition
 
@@ -39,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'guests.apps.GuestsConfig',
 ]
-
+LANGUAGE_SESSION_KEY = '_language'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bigday.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
@@ -116,7 +112,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOCAL_PATHS =['./locales/']
+LOCALE_PATHS = ['./bigday/locales/']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
--- wedding/views.py
+++ wedding/views.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render
+from django.utils import translation
+
 from guests.save_the_date import SAVE_THE_DATE_CONTEXT_MAP
 
 
