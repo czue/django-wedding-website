cfcbaac2d4f8caada97e4e331987e56d4f66a547
Alexander Taylor <31589434+dillwishlist@users.noreply.github.com>
2021-07-05 08:25:33 -0400

    Update settings.py
    Default to HTTPS for website

--- bigday/settings.py
+++ bigday/settings.py
@@ -144,7 +144,7 @@ DEFAULT_WEDDING_TEST_EMAIL = DEFAULT_WEDDING_FROM_EMAIL
 
 
 # This is used in links in save the date / invitations
-WEDDING_WEBSITE_URL = 'http://bigday.thehappycouple.com'
+WEDDING_WEBSITE_URL = 'https://thehappycouple.com'
 WEDDING_CC_LIST = []  # put email addresses here if you want to cc someone on all your invitations
 
 # change to a real email backend in production
